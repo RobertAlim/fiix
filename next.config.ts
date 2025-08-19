62130c925dc8bb3b9d42590682c5fc93cabbe0dd
RobertAlim <robert_alim@yahoo.com>
2025-08-19 21:20:59 +0800

    Adding Fix for the cameras in production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					// Allow camera for your origin. Remove entirely if you don't need to restrict.
+					{ key: "Permissions-Policy", value: "camera=(self)" },
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
