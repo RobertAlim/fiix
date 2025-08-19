ecb25ac7ee4405681c2153d3e2cb236cd528bf8f
RobertAlim <robert_alim@yahoo.com>
2025-08-19 15:53:23 +0800

    Fixing errors in compliance with vercel 15

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState, Suspense } from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
