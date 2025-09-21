44164abb56d641fb593a18de0d187b803f7c1e4c
Robert Alim <robert_alim@yahoo.com>
2025-09-21 23:48:36 +0800

    fix signature pad not working in production

--- components/SignaturePad.tsx
+++ components/SignaturePad.tsx
@@ -1,5 +1,5 @@
 // components/SignaturePad.tsx
-import React, { useRef, useState, forwardRef } from "react";
+import React, { useRef, useState, useEffect, forwardRef } from "react";
 import { Button } from "@/components/ui/button";
 import dynamic from "next/dynamic";
 import SignatureCanvas, { SignatureCanvasProps } from "react-signature-canvas";
@@ -26,6 +26,12 @@ export default function SignaturePad({
 }) {
 	const sigCanvasRef = useRef<SignatureCanvas | null>(null);
 	const [isEmpty, setIsEmpty] = useState(true);
+	const [isClient, setIsClient] = useState(false);
+
+	// This useEffect will only run on the client side after the component has mounted
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
 
 	const handleClear = () => {
 		sigCanvasRef.current?.clear();
@@ -48,18 +54,22 @@ export default function SignaturePad({
 	return (
 		<div className="p-4 border rounded-xl w-full max-w-md bg-white">
 			<p className="mb-2 font-medium">Sign below:</p>
-			<DynamicSignatureCanvas
-				penColor="black"
-				canvasProps={{
-					width: 400,
-					height: 150,
-					className: "border rounded-md border-gray-300",
-				}}
-				minWidth={1} // Minimum stroke width
-				maxWidth={1.5} // Maximum stroke width
-				ref={sigCanvasRef}
-				onEnd={() => setIsEmpty(false)}
-			/>
+
+			{isClient && (
+				<DynamicSignatureCanvas
+					penColor="black"
+					canvasProps={{
+						width: 400,
+						height: 150,
+						className: "border rounded-md border-gray-300",
+					}}
+					minWidth={1} // Minimum stroke width
+					maxWidth={1.5} // Maximum stroke width
+					ref={sigCanvasRef}
+					onEnd={() => setIsEmpty(false)}
+				/>
+			)}
+
 			<div className="mt-4 flex justify-between">
 				<Button variant={"destructive"} onClick={handleClear}>
 					Clear
