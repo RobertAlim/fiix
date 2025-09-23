3e037e50cb568f0477795ae202ffce410df24a85
Robert Alim <robert_alim@yahoo.com>
2025-09-23 16:36:10 +0800

    add components for adding signatories

---
+++ app/api/signatories/route.ts
@@ -0,0 +1,43 @@
+// app/api/signatories/route.ts
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/db"; // Adjust this path to your Drizzle client setup
+import { signatories } from "@/db/schema"; // Adjust this path to your Drizzle schema
+import { ensureError } from "@/lib/errors";
+
+export async function POST(req: NextRequest) {
+	try {
+		const signatory = await req.json();
+
+		const { clientId, firstName, lastName } = signatory;
+
+		await db
+			.insert(signatories)
+			.values({
+				firstName: firstName,
+				lastName: lastName,
+				clientId: Number(clientId),
+			})
+			.onConflictDoNothing({
+				target: [signatories.firstName, signatories.lastName],
+			})
+			.returning({ id: signatories.id }); // Get the ID of the newly inserted schedule
+
+		// If both inserts succeed, return success
+		return NextResponse.json(
+			{
+				message: "New signatory created successfully.",
+			},
+			{ status: 201 }
+		);
+	} catch (error: unknown) {
+		const err = ensureError(error);
+		console.error("Error saving signatory:", err.message);
+
+		// More specific error handling could be added here,
+		// e.g., checking for unique constraint violations if you had them.
+		return NextResponse.json(
+			{ message: err.message || "Internal server error." },
+			{ status: 500 }
+		);
+	}
+}
